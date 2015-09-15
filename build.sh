5241c65b9915b4f206ca8ebd6fe470fd6db02ad8
Andrew Martin <sublimino@gmail.com>
2015-09-15 10:47:46 +0100

    TEC-98 remove build server debug

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ exec docker run \
     /bin/sh -c "\
         cd /code && \
         printf \"@economist:registry=https://registry.npmjs.org/\n//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n\" > /root/.npmrc && \
-        npm i --unsafe-perm --verbose && \
+        npm i --unsafe-perm && \
         SAUCE_ACCESS_KEY=${SAUCE_ACCESS_KEY} npm t && \
         { [[ \"$(git rev-parse --abbrev-ref HEAD)\" == \"master\" ]] && npm run pages; } \
     ";
