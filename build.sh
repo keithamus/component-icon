77409bdcd9ad72635d2add3f9d7b0c3a52c9d5bc
Andrew Martin <sublimino@gmail.com>
2015-09-15 15:31:33 +0100

    TEC-98 post `npm run provision`

--- build.sh
+++ build.sh
@@ -7,9 +7,10 @@ exec docker run \
     -v $(pwd):/code \
     mhart/alpine-node \
     /bin/sh -c "\
+        apk --update add python && \
         cd /code && \
         printf \"@economist:registry=https://registry.npmjs.org/\n//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n\" > /root/.npmrc && \
-        npm i --unsafe-perm && \
+        npm i --unsafe-perm --verbose && \
         SAUCE_ACCESS_KEY=${SAUCE_ACCESS_KEY} npm t && \
         { [[ \"$(git rev-parse --abbrev-ref HEAD)\" == \"master\" ]] && npm run pages; } \
     ";
