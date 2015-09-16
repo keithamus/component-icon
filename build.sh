7eca57c68e424f836193c55cdabfb3d9a90664a5
Andrew Martin <sublimino@gmail.com>
2015-09-16 23:42:03 +0100

    TEC-95 add trap for build server

--- build.sh
+++ build.sh
@@ -5,11 +5,14 @@ SSH_KEY=${1:-/root/.ssh/id_rsa}
 [[ ${NPM_TOKEN:-} = '' ]] && { echo "NPM_TOKEN empty"; exit 1; }
 [[ ${SAUCE_ACCESS_KEY:-} = '' ]] && { echo "SAUCE_ACCESS_KEY empty"; exit 2; }
 
+docker pull sublimino/node4-base
+
 exec docker run \
     -v "${SSH_KEY}":/root/.ssh/id_rsa \
     -v $(pwd):/code \
     sublimino/node4-base \
     /bin/sh -cx "\
+        trap 'chmod 777 node_modules -R' EXIT && \
         cd /code && \
         umask 000 && \
         printf \"@economist:registry=https://registry.npmjs.org/\n//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n\" > ~/.npmrc && \
