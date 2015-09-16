8671c5289fe8f86bebfda7c386df08427a16398c
Andrew Martin <sublimino@gmail.com>
2015-09-16 14:25:11 +0100

    TEC-95 post `npm run provision`

--- build.sh
+++ build.sh
@@ -1,17 +1,22 @@
-#!/bin/bash -x
+#!/bin/bash
 
-SSH_KEY=${1:-/root/.ssh/id_rsa_deploy}
+SSH_KEY=${1:-/root/.ssh/id_rsa}
+
+[[ ${NPM_TOKEN:-} = '' ]] && { echo "NPM_TOKEN empty"; exit 1; }
+[[ ${SAUCE_ACCESS_KEY:-} = '' ]] && { echo "SAUCE_ACCESS_KEY empty"; exit 2; }
 
 exec docker run \
     -v "${SSH_KEY}":/root/.ssh/id_rsa \
     -v $(pwd):/code \
-    mhart/alpine-node \
+    node:4.0-slim \
     /bin/sh -c "\
-        apk --update add python && \
-        cd /code && \
+        apt-get update && apt-get install -y git python2.7 && \
         printf \"@economist:registry=https://registry.npmjs.org/\n//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n\" > /root/.npmrc && \
+        cd /code && \
         npm i --unsafe-perm --verbose && \
-        SAUCE_ACCESS_KEY=${SAUCE_ACCESS_KEY} npm t && \
-        { [[ \"$(git rev-parse --abbrev-ref HEAD)\" == \"master\" ]] && npm run pages; } \
+        echo SAUCE_USER=sublimino SAUCE_ACCESS_KEY=${SAUCE_ACCESS_KEY} npm t && \
+        { git config --global user.email 'ecprod@economist.com'; git config --global user.name 'GoCD'; true; } && \
+        { [ \"$(git rev-parse --abbrev-ref HEAD)\" = \"master\" ] && npm run pages; } \
+        echo 'Build finished' \
     ";
 
